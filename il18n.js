f70625cfe5a6825212a4b40a78ab36fc0464fa81
Luigi Cirelli <cirelliluigi5@gmail.com>
2023-07-10 16:03:47 +0200

    disperazione

--- il18n.js
+++ il18n.js
@@ -1,9 +1,17 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import Store from 'electron-store';
+import path from 'path';
 
 // Importa i file di traduzione per le diverse lingue
 import translationEN from './locales/en.json';
 import translationIT from './locales/it.json';
+
+const store = new Store();
+
+const defaultLanguageFilePath = path.join(store.path, 'cazz1-launcher', 'config.json');
+const defaultLanguageFile = store.get('defaultLanguageFile', defaultLanguageFilePath);
+const defaultLanguage = window.require(defaultLanguageFile).language;
 
 i18n.use(initReactI18next).init({
   resources: {
@@ -14,7 +22,7 @@ i18n.use(initReactI18next).init({
       translation: translationIT,
     },
   },
-  lng: 'it', // Imposta la lingua predefinita
+  lng: defaultLanguage, // Imposta la lingua predefinita
   fallbackLng: 'en', // Lingua di fallback in caso di mancanza di traduzioni
   interpolation: {
     escapeValue: false, // Permette l'uso di HTML nei testi tradotti
--- src/main/getOS.lua
+++ src/main/getOS.lua
@@ -1,19 +1,16 @@
 local json = require("cjson") -- Assicurati di avere il modulo JSON installato
-local os = require("os")
 
 local systeminfo = io.popen('systeminfo')
 local cmdout=systeminfo:read("*a")
 systeminfo:close()
 local osLanguage = string.match(cmdout, "..;")
 
-local username = os.getenv("USERNAME") -- Ottieni il nome utente del PC
-print(username)
 local prefs = {
   language = osLanguage:gsub(";", "")
 }
 
-local prefsFilePath = "C:/Users/" .. username .. "/AppData/Roaming/cazz1-launcher/config.json" -- Sostituisci con il percorso corretto del file
-print(prefsFilePath)
+local prefsFilePath = os.getenv("AppData").."/cazz1-launcher/config.json" -- Sostituisci con il percorso corretto del file
+
 local file = io.open(prefsFilePath, "w")
 if file then
   file:write(json.encode(prefs))
