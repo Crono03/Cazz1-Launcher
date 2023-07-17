d14fe1c6f8b5a70d78b787495bbe14a30939748b
Giorgio <iggoroi.g@gmail.com>
2023-07-17 09:58:11 +0200

    More error handling

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use fancy_regex::Regex;
-use surrealdb::{engine::remote::ws::Ws, sql::Thing, Surreal};
+use surrealdb::{
+    engine::remote::ws::{self, Ws},
+    sql::Thing,
+    Surreal,
+};
 
 #[derive(Debug, thiserror::Error)]
 enum Error {
@@ -24,6 +28,8 @@ enum Error {
     InvalidPasswordLenght(String),
     #[error("{0}")]
     InvalidPassword(String),
+    #[error("{0}")]
+    Connection(String),
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
@@ -59,13 +65,13 @@ async fn signup(
     if password != confirm_password {
         return Err(Error::PasswordNotEquals("notSamePassword".to_string()));
     }
-    let password_regex = Regex::new(r#"^((?!.*[\s"';])(?=.*[A-Z])(?=.*\d).{8,255})$"#).unwrap();
     if password.len() < 8 {
         return Err(Error::InvalidPasswordLenght("tooShortPassword".to_string()));
     }
     if password.len() > 255 {
         return Err(Error::InvalidPasswordLenght("tooLognPassword".to_string()));
     }
+    let password_regex = Regex::new(r#"^((?!.*[\s"';])(?=.*[A-Z])(?=.*\d).{8,255})$"#).unwrap();
     if !password_regex.is_match(password).unwrap() {
         return Err(Error::InvalidPassword("invalidPassword".to_string()));
     }
@@ -80,7 +86,20 @@ async fn signup(
     if !username_regex.is_match(username).unwrap() {
         return Err(Error::InvalidUsername("invalidUsername".to_string()));
     }
-    let db = Surreal::new::<Ws>("127.0.0.1:8000").await?;
+    let connection: Result<Surreal<ws::Client>, surrealdb::Error> = Surreal::new::<Ws>("127.0.0.1:8000").await;
+    let db: Surreal<ws::Client>;
+    match connection {
+        Ok(client) => db = client,
+        Err(surrealdb::Error::Api(surrealdb::error::Api::Ws(_))) => {
+            return Err(Error::Connection("wSError".to_string()))
+        }
+        Err(surrealdb::Error::Api(_)) => {
+            return Err(Error::Connection("connectionError".to_string()))
+        }
+        Err(surrealdb::Error::Db(_)) => {
+            return Err(Error::Connection("connectionError".to_string()))
+        }
+    }
     db.signin(surrealdb::opt::auth::Root {
         username: "root",
         password: "pic1",
